ae45c174be28db1f8a9da9c1bcbb3bb4fece13d1
Maria Laura <134738115+MariaLPMotta@users.noreply.github.com>
2025-11-02 11:16:46 -0300

    Refactor database queries to use db config

--- backend/models/funcionario.model.js
+++ backend/models/funcionario.model.js
@@ -1,43 +1,30 @@
-const pool = require('../config/db.config');
+const db = require('../config/db.config');
 
 const Funcionario = {
     getAll: async () => {
-        const res = await pool.query(`
-            SELECT * FROM Funcionario WHERE Removido = FALSE
-        `);
-        return res.rows;
+        return db.query("SELECT * FROM Funcionario WHERE Removido = FALSE");
     },
 
     getById: async (id) => {
-        const res = await pool.query(`
-            SELECT * FROM Funcionario WHERE ID = $1 AND Removido = FALSE
-        `, [id]);
-        return res.rows[0];
+        return db.query("SELECT * FROM Funcionario WHERE ID = $1 AND Removido = FALSE", [id]);
     },
 
     create: async ({ Nome, Cargo, DataContratacao, Salario }) => {
-        const res = await pool.query(`
-            INSERT INTO Funcionario (Nome, Cargo, DataContratacao, Salario)
-            VALUES ($1, $2, $3, $4)
-            RETURNING ID
-        `, [Nome, Cargo, DataContratacao, Salario]);
-        return { id: res.rows[0].id };
+        return db.query(
+            "INSERT INTO Funcionario (Nome, Cargo, DataContratacao, Salario) VALUES ($1, $2, $3, $4) RETURNING *",
+            [Nome, Cargo, DataContratacao, Salario]
+        );
     },
 
     update: async ({ ID, Nome, Cargo, DataContratacao, Salario }) => {
-        const res = await pool.query(`
-            UPDATE Funcionario
-            SET Nome = $1, Cargo = $2, DataContratacao = $3, Salario = $4
-            WHERE ID = $5 AND Removido = FALSE
-        `, [Nome, Cargo, DataContratacao, Salario, ID]);
-        return res;
+        return db.query(
+            "UPDATE Funcionario SET Nome = $1, Cargo = $2, DataContratacao = $3, Salario = $4 WHERE ID = $5 AND Removido = FALSE RETURNING *",
+            [Nome, Cargo, DataContratacao, Salario, ID]
+        );
     },
 
     delete: async (ID) => {
-        const res = await pool.query(`
-            UPDATE Funcionario SET Removido = TRUE WHERE ID = $1
-        `, [ID]);
-        return res;
+        return db.query("UPDATE Funcionario SET Removido = TRUE WHERE ID = $1 RETURNING *", [ID]);
     }
 };
 
