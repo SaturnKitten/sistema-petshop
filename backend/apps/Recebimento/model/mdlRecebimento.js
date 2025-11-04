6f64d2a4986bc56e464f3f881048bd7d2a2c4bb5
Maria Laura <134738115+MariaLPMotta@users.noreply.github.com>
2025-11-03 22:43:30 -0300

    Update mdlRecebimento.js

--- backend/apps/Recebimento/model/mdlRecebimento.js
+++ backend/apps/Recebimento/model/mdlRecebimento.js
@@ -1,99 +1,106 @@
 const db = require("../../../database/databaseconfig");
 
-const getAllRecebimentos = async () => {
-    return (
-        await db.query(
-            "SELECT *, (SELECT descricao FROM contareceber WHERE id = r.id_contareceber) " +
-            "FROM recebimento r WHERE r.removido = false ORDER BY r.datarecebimento DESC"
-        )
-    ).rows;
+const GetAllRecebimentos = async () => {
+  return (
+    await db.query(
+      "SELECT ID, Descricao, ID_ContaReceber, DataRecebimento, ValorRecebido, MetodoPagamento " +
+      "FROM Recebimento WHERE Removido = FALSE ORDER BY DataRecebimento DESC"
+    )
+  ).rows;
 };
 
-const getRecebimentoByID = async (recebimentoID) => {
-    return (
-        await db.query(
-            "SELECT *, (SELECT descricao FROM contareceber WHERE id = r.id_contareceber) " +
-            "FROM recebimento r WHERE r.id = $1 AND r.removido = false",
-            [recebimentoID]
-        )
-    ).rows;
+const GetRecebimentoByID = async (recebimentoIDPar) => {
+  return (
+    await db.query(
+      "SELECT ID, Descricao, ID_ContaReceber, DataRecebimento, ValorRecebido, MetodoPagamento " +
+      "FROM Recebimento WHERE ID = $1 AND Removido = FALSE ORDER BY DataRecebimento DESC",
+      [recebimentoIDPar]
+    )
+  ).rows;
 };
 
-const insertRecebimento = async (recebimentoREG) => {
-    let linhasAfetadas;
-    let msg = "ok";
-    try {
-        linhasAfetadas = (
-            await db.query(
-                "INSERT INTO recebimento " +
-                "VALUES (DEFAULT, $1, $2, $3, $4, $5, false)",
-                [
-                    recebimentoREG.descricao,
-                    recebimentoREG.id_contareceber,
-                    recebimentoREG.datarecebimento,
-                    recebimentoREG.valorrecebido,
-                    recebimentoREG.metodopagamento
-                ]
-            )
-        ).rowCount;
-    } catch (error) {
-        msg = "[mdlRecebimento|insertRecebimento] " + error.detail;
-        linhasAfetadas = -1;
-    }
-    return { msg, linhasAfetadas };
+const InsertRecebimentos = async (recebimentoREGPar) => {
+  let linhasAfetadas;
+  let msg = "ok";
+
+  try {
+    linhasAfetadas = (
+      await db.query(
+        "INSERT INTO Recebimento " +
+        "VALUES (DEFAULT, $1, $2, $3, $4, $5, FALSE)",
+        [
+          recebimentoREGPar.descricao,
+          recebimentoREGPar.id_contareceber,
+          recebimentoREGPar.datarecebimento,
+          recebimentoREGPar.valorrecebido,
+          recebimentoREGPar.metodopagamento
+        ]
+      )
+    ).rowCount;
+  } catch (error) {
+    msg = "[mdlRecebimento|InsertRecebimentos] " + error.detail;
+    linhasAfetadas = -1;
+  }
+
+  return { msg, linhasAfetadas };
 };
 
-const updateRecebimento = async (recebimentoREG) => {
-    let linhasAfetadas;
-    let msg = "ok";
-    try {
-        linhasAfetadas = (
-            await db.query(
-                "UPDATE recebimento SET " +
-                "descricao = $2, " +
-                "id_contareceber = $3, " +
-                "datarecebimento = $4, " +
-                "valorrecebido = $5, " +
-                "metodopagamento = $6 " +
-                "WHERE id = $1 AND removido = false",
-                [
-                    recebimentoREG.id,
-                    recebimentoREG.descricao,
-                    recebimentoREG.id_contareceber,
-                    recebimentoREG.datarecebimento,
-                    recebimentoREG.valorrecebido,
-                    recebimentoREG.metodopagamento
-                ]
-            )
-        ).rowCount;
-    } catch (error) {
-        msg = "[mdlRecebimento|updateRecebimento] " + error.detail;
-        linhasAfetadas = -1;
-    }
-    return { msg, linhasAfetadas };
+const UpdateRecebimentos = async (recebimentoREGPar) => {
+  let linhasAfetadas;
+  let msg = "ok";
+
+  try {
+    linhasAfetadas = (
+      await db.query(
+        "UPDATE Recebimento SET " +
+        "Descricao = $2, " +
+        "ID_ContaReceber = $3, " +
+        "DataRecebimento = $4, " +
+        "ValorRecebido = $5, " +
+        "MetodoPagamento = $6 " +
+        "WHERE ID = $1",
+        [
+          recebimentoREGPar.id,
+          recebimentoREGPar.descricao,
+          recebimentoREGPar.id_contareceber,
+          recebimentoREGPar.datarecebimento,
+          recebimentoREGPar.valorrecebido,
+          recebimentoREGPar.metodopagamento
+        ]
+      )
+    ).rowCount;
+  } catch (error) {
+    msg = "[mdlRecebimento|UpdateRecebimentos] " + error.detail;
+    linhasAfetadas = -1;
+  }
+
+  return { msg, linhasAfetadas };
 };
 
-const deleteRecebimento = async (recebimentoREG) => {
-    let linhasAfetadas;
-    let msg = "ok";
-    try {
-        linhasAfetadas = (
-            await db.query(
-                "UPDATE recebimento SET removido = true WHERE id = $1",
-                [recebimentoREG.id]
-            )
-        ).rowCount;
-    } catch (error) {
-        msg = "[mdlRecebimento|deleteRecebimento] " + error.detail;
-        linhasAfetadas = -1;
-    }
-    return { msg, linhasAfetadas };
+const DeleteRecebimentos = async (recebimentoREGPar) => {
+  let linhasAfetadas;
+  let msg = "ok";
+
+  try {
+    linhasAfetadas = (
+      await db.query(
+        "UPDATE Recebimento SET Removido = TRUE WHERE ID = $1",
+        [recebimentoREGPar.id]
+      )
+    ).rowCount;
+  } catch (error) {
+    msg = "[mdlRecebimento|DeleteRecebimentos] " + error.detail;
+    linhasAfetadas = -1;
+  }
+
+  return { msg, linhasAfetadas };
 };
 
 module.exports = {
-    getAllRecebimentos,
-    getRecebimentoByID,
-    insertRecebimento,
-    updateRecebimento,
-    deleteRecebimento,
+  GetAllRecebimentos,
+  GetRecebimentoByID,
+  InsertRecebimentos,
+  UpdateRecebimentos,
+  DeleteRecebimentos
 };
+
