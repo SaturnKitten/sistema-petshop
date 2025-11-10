df28babe9b55f28728484108728a1e6ce448196a
IzadoraMS <izadora.macedo2205@gmail.com>
2025-11-10 19:10:36 -0300

    Atualizando routes, views, app.js

--- petshopfrontNode/routes/rte_funcionarios.js
+++ petshopfrontNode/routes/rte_funcionarios.js
@@ -1,23 +1,27 @@
-var express =require('express');
-var alunosApp =require("../app/funcionario/controller/ctlFuncionario")
+var express = require('express');
+var alunosApp = require("../app/funcionario/controller/ctlFuncionario")
 var router = express.Router();
 
-functionauthenticationMiddleware(req, res,next) {
+//Função necessária para evitar que usuários não autenticados acessem o sistema.
+function authenticationMiddleware(req, res, next) {
 
-isLogged= req.session.isLogged;
+  // Verificar se existe uma sessão válida.
+  isLogged = req.session.isLogged;
 
-if(!isLogged) {
-  res.redirect("/Login");
- }
-();
+  if (!isLogged) {
+    res.redirect("/Login");
+  }
+  next();
 };
- 
-router.get('/',authenticationMiddleware, funcionarioApp.getAllFuncionario);
-router.get('/InsertFuncionario',authenticationMiddleware, funcionarioApp.insertFuncionario);
-router.get('/ViewFuncionarios/:id/:oper',authenticationMiddleware, funcionarioApp.viewFuncionario);
 
-router.post('/InsertFuncionario',authenticationMiddleware, funcionarioApp.insertFuncionario);
-router.post('/DeleteFuncionario',authenticationMiddleware, funcionarioApp.DeleteFuncionario);
-router.post('/ViewFuncionario',authenticationMiddleware, funcionarioApp.viewFuncionario);
+/* GET métodos */
+router.get('/', authenticationMiddleware, funcionarioApp.getAllFuncionario);
+router.get('/InsertFuncionario', authenticationMiddleware, funcionarioApp.insertFuncionario);
+router.get('/ViewFuncionarios/:id/:oper', authenticationMiddleware, funcionarioApp.viewFuncionario);
 
- module.exports = router;
+/* POST métodos */
+router.post('/InsertFuncionario', authenticationMiddleware, funcionarioApp.insertFuncionario);
+router.post('/DeleteFuncionario', authenticationMiddleware, funcionarioApp.deleteFuncionario);
+router.post('/ViewFuncionario', authenticationMiddleware, funcionarioApp.viewFuncionario);
+
+module.exports = router;
